ea1ddaa3d08527cfb8b7e9cbcec5dd9302536cc2
Lindsay Block <lindsay.block05@gmail.com>
2018-03-05 16:32:12 -0700

    stuff

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,43 +1,31 @@
-var products = []
+var arrProducts = [];
 
 $(document).ready(function() {
   $.ajax({
-    url: 'http://json-server.devpointlabs.com/api/v1/products',
-    method: 'GET'
-  }).done( function(products) {
-    var productArea = $('#products')
-    productArea.empty()
-    products.forEach( function(product) {
-      var div =
-      '<div id=' +
-      product.id +
-      ' class="four wide column ui card"><p>' +
-      product.name +
-      '</p><p>' +
-      product.description +
-      '</p><div class="extra content"><div class="ui three buttons"><div class="ui basic blue button" onClick="sayHello(' +
-      product.id +
-      ')">Show</div><div class="ui basic green button">Edit</div><div class="ui basic red button">Delete</div></div></div></div>';
-      productArea.append(div);
-    })
+    url: "http://json-server.devpointlabs.com/api/v1/products",
+    method: "GET"
+  }).done(function(products) {
+    arrProducts = products
+    show(arrProducts)
+  });
+});
+
+$(document).on('click', '.card', function(){
+
+  $.ajax({
+    url: `http://json-server.devpointlabs.com/api/v1/products/${this.id}`,
+    method: "GET"
+  }).done( function(product){
+    arrProducts = [product]
+    show(arrProducts);
   })
 })
 
-function sayHello(id) {
-  $.ajax({
-    url: `http://json-server.devpointlabs.com/api/v1/products/${id}`,
-    method: 'GET'
-  }).done( function(product) {
-    var productArea = $('#products')
-    productArea.empty();
-    var div =
-    '<div id=' +
-    product.id +
-    ' class="four wide column ui card"><p>' +
-    product.name +
-    '</p><p>' +
-    product.description +
-    '</p>';
+function show(products){
+  var productArea = $("#products");
+  productArea.empty();
+  products.forEach(function(product) {
+    var div = "<div id=" + product.id + ' class="four wide column ui card"><p>' + product.name + "</p><p>" + product.description + '</p><div class="extra content"><div class="ui three buttons"><div class="ui basic blue button">Show</div><div class="ui basic green button">Edit</div><div class="ui basic red button">Delete</div></div></div></div>';
     productArea.append(div);
-  })
+  });
 }
